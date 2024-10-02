24ec4bff9854976d83c538c25c8527acdcf3e07c
Grace Smith <gracesmith6504@gmail.com>
2024-10-02 19:30:34 +0100

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-public class CircleMetrics {
+public class BMI {
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner( System.in );
-		System.out.print("What is the radius of the circle? ");
-		double radius = input.nextDouble();
-		input.close();
-
-		double diameter = radius * 2;
-		double circumference = 2 * Math.PI * radius;
-		double area = Math.PI * radius * radius;
+		System.out.println("What is your weight in kg?");
+		double weight = input.nextDouble();
+		System.out.println("What is your height in metres?");
+		double height = input.nextDouble();
 		
-		System.out.println("For a circle of radius " + radius +
-			"\n Diameter = " + diameter + "\n Circumference = " + circumference + 
-			"\n Area = " + area );
+		double bmi = weight / (height*height);
+		
+		System.out.println("Your BMI is " + bmi );
 	}
+
 }
